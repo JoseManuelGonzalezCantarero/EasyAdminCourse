bd010a4fc5ffa5805770945c85ffd9601dc3bb03
Manu Gonzalez <jgonzalezcantarero@gmail.com>
2018-01-22 17:29:31 +0100

    Conditional Actions

--- src/AppBundle/Event/EasyAdminSubscriber.php
+++ src/AppBundle/Event/EasyAdminSubscriber.php
@@ -7,23 +7,36 @@ use AppBundle\Entity\User;
 use JavierEguiluz\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class EasyAdminSubscriber implements EventSubscriberInterface
 {
 
     private $tokenStorage;
+    private $authorizationChecker;
 
-    public function __construct(TokenStorageInterface $tokenStorage)
+    public function __construct(TokenStorageInterface $tokenStorage, AuthorizationCheckerInterface $authorizationChecker)
     {
         $this->tokenStorage = $tokenStorage;
+        $this->authorizationChecker = $authorizationChecker;
     }
 
     public static function getSubscribedEvents()
     {
         return [
             EasyAdminEvents::PRE_UPDATE => 'onPreUpdate',
+            EasyAdminEvents::PRE_EDIT => 'onPreEdit',
         ];
+    }
+
+    public function onPreEdit(GenericEvent $event)
+    {
+        $config = $event->getSubject();
+        if ($config['class'] == User::class) {
+            $this->denyAccessUnlessSuperAdmin();
+        }
     }
 
     public function onPreUpdate(GenericEvent $event)
@@ -37,6 +50,13 @@ class EasyAdminSubscriber implements EventSubscriberInterface
             }
 
             $entity->setLastUpdatedBy($user);
+        }
+    }
+
+    private function denyAccessUnlessSuperAdmin()
+    {
+        if (!$this->authorizationChecker->isGranted('ROLE_SUPERADMIN')) {
+            throw new AccessDeniedException();
         }
     }
 
--- src/AppBundle/Twig/EasyAdminExtension.php
+++ src/AppBundle/Twig/EasyAdminExtension.php
@@ -4,9 +4,19 @@ namespace AppBundle\Twig;
 
 
 use AppBundle\Entity\Genus;
+use AppBundle\Entity\User;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class EasyAdminExtension extends \Twig_Extension
 {
+
+    private $authorizationChecker;
+
+    public function __construct(
+        AuthorizationCheckerInterface $authorizationChecker
+    ) {
+        $this->authorizationChecker = $authorizationChecker;
+    }
 
     public function getFilters()
     {
@@ -22,6 +32,12 @@ class EasyAdminExtension extends \Twig_Extension
     {
         if ($item instanceof Genus && $item->getIsPublished()) {
             unset($itemActions['delete']);
+        }
+
+        if ($item instanceof User
+            && ! $this->authorizationChecker->isGranted('ROLE_SUPERADMIN')
+        ) {
+            unset($itemActions['edit']);
         }
 
         return $itemActions;
